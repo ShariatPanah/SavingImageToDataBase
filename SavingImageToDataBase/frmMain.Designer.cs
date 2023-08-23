bb1fc87029b96e7c1d6364922293ba3da214df8e
Sirwan ShariatPanah <er.sirwan@live.com>
2023-08-23 13:22:17 +0330

    Add project files.

---
+++ SavingImageToDataBase/frmMain.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SavingImageToDataBase
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbIndices = new System.Windows.Forms.ComboBox();
+            this.btnRetrieve = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pcbPictures = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnSaveToPc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPictures)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbIndices
+            // 
+            this.cmbIndices.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmbIndices.FormattingEnabled = true;
+            this.cmbIndices.Location = new System.Drawing.Point(118, 256);
+            this.cmbIndices.Name = "cmbIndices";
+            this.cmbIndices.Size = new System.Drawing.Size(166, 21);
+            this.cmbIndices.TabIndex = 11;
+            this.cmbIndices.Text = "Image indices";
+            // 
+            // btnRetrieve
+            // 
+            this.btnRetrieve.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRetrieve.Location = new System.Drawing.Point(12, 250);
+            this.btnRetrieve.Name = "btnRetrieve";
+            this.btnRetrieve.Size = new System.Drawing.Size(100, 31);
+            this.btnRetrieve.TabIndex = 10;
+            this.btnRetrieve.Text = "Retrieve Picture";
+            this.btnRetrieve.UseVisualStyleBackColor = true;
+            this.btnRetrieve.Click += new System.EventHandler(this.btnRetrieve_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(290, 250);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 31);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear Picture";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // pcbPictures
+            // 
+            this.pcbPictures.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pcbPictures.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pcbPictures.Location = new System.Drawing.Point(12, 12);
+            this.pcbPictures.Name = "pcbPictures";
+            this.pcbPictures.Size = new System.Drawing.Size(590, 232);
+            this.pcbPictures.TabIndex = 8;
+            this.pcbPictures.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(396, 250);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 31);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save Image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelect.Location = new System.Drawing.Point(502, 250);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(100, 31);
+            this.btnSelect.TabIndex = 6;
+            this.btnSelect.Text = "Select Image";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnSaveToPc
+            // 
+            this.btnSaveToPc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveToPc.Location = new System.Drawing.Point(12, 287);
+            this.btnSaveToPc.Name = "btnSaveToPc";
+            this.btnSaveToPc.Size = new System.Drawing.Size(590, 31);
+            this.btnSaveToPc.TabIndex = 12;
+            this.btnSaveToPc.Text = "Save Image To PC";
+            this.btnSaveToPc.UseVisualStyleBackColor = true;
+            this.btnSaveToPc.Click += new System.EventHandler(this.btnSaveToPc_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 327);
+            this.Controls.Add(this.btnSaveToPc);
+            this.Controls.Add(this.cmbIndices);
+            this.Controls.Add(this.btnRetrieve);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.pcbPictures);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSelect);
+            this.MinimumSize = new System.Drawing.Size(630, 322);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPictures)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbIndices;
+        private System.Windows.Forms.Button btnRetrieve;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox pcbPictures;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnSaveToPc;
+    }
+}
+
---
+++ SavingImageToDataBase/frmMain.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SavingImageToDataBase
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public static string strConnectionString = "Data Source=.;Initial Catalog=DBImage;Integrated Security=True";
+
+        public byte[] retrievePictureByID(int _id)
+        {
+            SqlConnection Connection = new SqlConnection(strConnectionString);
+            SqlCommand Command = new SqlCommand();
+
+            byte[] arrPicture = null;
+            try
+            {
+                Connection.Open();
+                Command.Connection = Connection;
+                Command.CommandText = "Select * From tbl_Image Where ID=@id";
+
+                Command.Parameters.AddWithValue("@id", _id);
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    arrPicture = (byte[])Reader["Image"];
+                }
+
+                return arrPicture;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return arrPicture;
+            }
+            finally
+            {
+                if (Connection.State == ConnectionState.Open)
+                {
+                    Connection.Close();
+                }
+            }
+        }
+
+        public void updateComboBox()
+        {
+            try
+            {
+                cmbIndices.Items.Clear();
+                foreach (int item in getIndices())
+                {
+                    cmbIndices.Items.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public int[] getIndices()
+        {
+            SqlConnection Connection = new SqlConnection(strConnectionString);
+            SqlCommand Command = new SqlCommand();
+
+            int[] indices = null;
+            try
+            {
+                Connection.Open();
+                Command.Connection = Connection;
+                Command.CommandText = "Select Count(*) From tbl_Image";
+
+                indices = new int[(int)Command.ExecuteScalar()];
+                Command.CommandText = "Select id From tbl_Image";
+
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                for (int i = 0; Reader.Read(); i++)
+                {
+                    indices[i] = (int)Reader["id"];
+                }
+
+                return indices;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return indices;
+            }
+            finally
+            {
+                if (Connection.State == ConnectionState.Open)
+                {
+                    Connection.Close();
+                }
+            }
+        }
+
+        public byte[] getPicturesByte()
+        {
+            Bitmap oBitmap = (Bitmap)pcbPictures.Image;
+            System.IO.MemoryStream oMemoryStream = new System.IO.MemoryStream();
+            oBitmap.Save(oMemoryStream, oBitmap.RawFormat);
+
+            byte[] arrPicture = oMemoryStream.GetBuffer();
+            return arrPicture;
+        }
+
+        public void insertPicture(byte[] _picture)
+        {
+            SqlConnection Connection = new SqlConnection(strConnectionString);
+            SqlCommand Command = new SqlCommand();
+            try
+            {
+                Connection.Open();
+                Command.Connection = Connection;
+                Command.CommandText = "Insert into tbl_Image Select Image=@Img";
+
+                Command.Parameters.AddWithValue("@Img", _picture);
+                Command.ExecuteNonQuery();
+
+                MessageBox.Show("Succeed!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (Connection.State == ConnectionState.Open)
+                {
+                    Connection.Close();
+                }
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oOpenFileDialog = new OpenFileDialog();
+            oOpenFileDialog.Filter = "All Graphic Types|*.jpg;*.jpeg;*.png;*.bmp;*.tiff;*.gif";
+
+            if (oOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pcbPictures.Image = Image.FromFile(oOpenFileDialog.FileName);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pcbPictures.Image = null;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            insertPicture(getPicturesByte());
+            updateComboBox();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            updateComboBox();
+        }
+
+        private void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            if (cmbIndices.Text != "Image indices")
+            {
+                System.IO.MemoryStream oMemoryStream = new System.IO.MemoryStream(retrievePictureByID(Convert.ToInt32(cmbIndices.SelectedItem)));
+                pcbPictures.Image = Image.FromStream(oMemoryStream);
+            }
+            else
+            {
+                MessageBox.Show("Please Select an Image Number");
+            }
+        }
+
+        private void btnSaveToPc_Click(object sender, EventArgs e)
+        {
+            Bitmap oBitmap = new Bitmap(pcbPictures.Image);
+
+            oBitmap.Save(Application.StartupPath + "12.png", System.Drawing.Imaging.ImageFormat.Png);
+            //pcbPictures.Image.Save(Application.StartupPath + "12.jpg", System.Drawing.Imaging.ImageFormat)
+        }
+    }
+}
